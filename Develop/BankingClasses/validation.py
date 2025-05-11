2dc8e3d4d703fc413319eff76246992a4fcd18c7
Matt Mullen <mhmullen@icloud.com>
2025-05-11 13:46:08 -0700

    added validation todos

--- Develop/BankingClasses/validation.py
+++ Develop/BankingClasses/validation.py
@@ -5,9 +5,12 @@ class Validation:
     @staticmethod
     # TODO: Implement the validate_email method using the staticmethod decorator.
     # TODO: The method should take an email parameter and return True if the email contains an "@" symbol.
+    def validate_email(email):
+        return "@" in email
 
     @staticmethod
     # TODO: Implement the validate_password method using the staticmethod decorator.
+    def validate_password(password):
         """
         This method validates a password based on the following criteria:
         - The password must be at least 8 characters long.
@@ -22,6 +25,8 @@ class Validation:
         """
         # TODO: Implement the password length validation logic.
         # TODO: Check if the password is at least 8 characters long if not return False.
+        if len(password) < 8:
+            return False
 
 
         # TODO: Set the initial values for uppercase, lowercase, digit, and special characters to False.
@@ -29,11 +34,23 @@ class Validation:
         # TODO: Has at least one lowercase letter
         # TODO: Has at least one digit
         # TODO: Has at least one special character
+        has_upper = False
+        has_lower = False
+        has_digit = False
+        has_special = False
         special_characters = "!@#$%^&*"
 
         for char in password:
         # TODO: Use if/elif/else statements to check the character type.
         # TODO: Set the corresponding variable to True if it fits the criteria.
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+        elif char in special_characters:
+            has_special = True
 
         # Return the boolean value based on the conditions.
         return has_upper and has_lower and has_digit and has_special
